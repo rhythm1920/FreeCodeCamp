62b5bf9f838435ab3ee1cfabd2de9ba4e23e5058
unknown <rhythm1920@gmail.com>
2020-07-06 19:14:42 +0530

    Reformed Text Component

--- front-end-1/my-app/src/App.js
+++ front-end-1/my-app/src/App.js
@@ -3,6 +3,20 @@ import quotes from "./quotes.json";
 //import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
+class Text extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div id="text">
+        <h3>{quotes.quotes[0]["quote"]}</h3>
+      </div>
+    );
+  }
+}
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -12,9 +26,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="text">
-          <h3>{quotes.quotes[0]["quote"]}</h3>
-        </div>
+        <Text />
       </div>
     );
   }
