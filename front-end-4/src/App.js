045be89f91b9e54368e99a33da8703f425d883f7
unknown <rhythm1920@gmail.com>
2020-07-14 18:52:03 +0530

    added store.js

--- front-end-4/src/App.js
+++ front-end-4/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 class App extends React.Component {
   constructor(props) {
---
+++ front-end-4/src/store.js
@@ -0,0 +1,6 @@
+import { connect } from "react-redux";
+import { createStore } from "redux";
+let reducer = () => {};
+let store = createStore(reducer);
+
+export default store;
